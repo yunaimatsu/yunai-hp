ea0fbf22647316f82b458c413aeee56656fc1ac4
root <root@MatsuuraYoshihiro>
2024-02-05 16:03:02 +0900

    setup 2

--- microblog/microblog/settings.py
+++ microblog/microblog/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles','blog'
+    'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
