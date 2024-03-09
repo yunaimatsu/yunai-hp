2c1541c9429e6774f659bb96b01549edbe1d682a
yunaimatsu <yunaimatsu@gmail.com>
2024-03-09 11:04:19 +0900

    `file_upload.html` created

--- dj-hp/prj_hp/app_hp/views.py
+++ dj-hp/prj_hp/app_hp/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 from .models import Langinf
 from django.utils.translation import gettext_lazy as _
 
 # Create your views here.
+
+def file_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'file_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+
 def home(request):
     my_traslatable_string = _("This is a translatable string.")
     return render(request, "home.html", {'my_translatable_string': my_traslatable_string})
@@ -21,8 +33,8 @@ def lthome(request):
     return render(request, "lang-typo.html")
 
 def ie(request):
-	infos = Langinf.objects.filter()
-	return render(request, "lang-typo/ie.html", {"infos": infos})
+    infos = Langinf.objects.filter()
+    return render(request, "lang-typo/ie.html", {"infos": infos})
 
 def txhome(request):
 	return render(request, "taxonomy/tx-home.html")
