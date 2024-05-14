7466aa47fa3b8405ce0ab5e82517a19a82ea628b
Matsuura Yoshihiro <yunaimatsu@gmail.com>
2024-05-15 00:29:18 +0900

    Make framework

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,25 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-Add
+<div class="navbar">
+    <a href="/cs">Computer Science</a>
+    <a href="/economics">Economcis</a>
+    <a href="/linguistics">Linguistics</a>
+</div>
+
+<style>
+    .navbar {
+        display: grid;
+        gap: 1rem;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: 1fr;
+    }
+    h1 {
+        color: #333;
+    }
+    p {
+        color: #666;
+    }
+    a {
+        color: #0070f3;
+    }
+</style>
