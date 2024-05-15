230766c79afa37a994ce1c6be049acf73d151f30
Matsuura Yoshihiro <yunaimatsu@gmail.com>
2024-05-15 20:33:49 +0900

    Add layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,75 @@
+<script>
+
+</script>
+<div class="navbar">
+    <p id="title">yunai</p>
+
+    <nav class="nav-menu">
+        <a href="/cs">Computer Science</a>
+        <a href="/economics">Economcis</a>
+        <a href="/linguistics">Linguistics</a>
+        <a href="mailto:yunaimatsu@gmail.com">Email</a>
+        <a href="https://github.com/yunaimatsu">GitHub</a>
+        <a href="https://twitter.com/supegoroshiago">Twitter</a>
+    </nav>
+</div>
+
+<slot />
+
+<footer>
+    <p style="font-family: 'Times New Roman', Times, serif;">yunai official</p>
+    <a href="mailto:yunaimatsu@gmail.com">Email</a>
+    
+    <a href="https://github.com/yunaimatsu">GitHub</a>
+    <a href="https://twitter.com/supegoroshiago">Twitter</a>
+</footer>
+
+<style>
+    .navbar {
+        background-color: #333;
+        color: white;
+        font-family: 'Roboto', serif;
+        padding: 0.5em;
+        text-align: center;
+        margin: 0;
+    }
+
+    #title {
+        font-size: 3em;
+    }
+
+    p {
+        margin: 0;
+    }
+
+    footer {
+        background-color: #EEEEEE;
+        color: #303030;
+        font-size: 1.5em;
+        font-family: 'Roboto', serif;
+        padding: 0.5em;
+        margin: 0;
+    }
+
+    .navbar {
+        display: flex;
+        gap: 1rem;
+        justify-content: center;
+        align-items: center;
+        padding: 1rem;
+    }
+
+    a {
+        color: #0077cc;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #005999;
+    }
+
+    .nav-menu {
+        display: flex;
+        gap: 1rem;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,50 +1,33 @@
 <main>
     <title>yunai official</title>
     <h1 class="title">Make the world more confortable for all humanity</h1>
-    <div class="navbar">
-        <a href="/cs">Computer Science</a>
-        <a href="/economics">Economcis</a>
-        <a href="/linguistics">Linguistics</a>
-    </div>
 </main>
 
 <style>
     * {
         font-family: 'Roboto', sans-serif;
     }
+
     main {
         background-color: #EFEFEF;
         border-color: #333;
         border-radius: 8px;
     }
+
     .title {
         color: #333;
         text-align: center;
         margin-top: 1em;
     }
-    .navbar {
-        display: flex;
-        gap: 1rem;
-        justify-content: center;
-        align-items: center;    
-        padding: 1rem;
-        border-radius: 8px;
-    }
+
     h1 {
         color: #333;
         font-size: 2rem;
         margin-bottom: 1rem;
     }
+
     /* p {
         color: #666;
         margin-bottom: 2rem;
     } */
-    a {
-        color: #0077cc;
-        text-decoration: none;
-    }
-
-    a:hover {
-        color: #005999;
-    }
 </style>
